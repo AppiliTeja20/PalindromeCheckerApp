9ea0653fa6d728af729087d3a25f573035fb7011
AppiliTeja20 <appilitejarayal81@gmail.com>
2026-02-24 15:13:18 +0530

    AppiliTeja20 Added UC9-Recursive

---
+++ App/src/UseCase9PalindromeCheckerApp.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class UseCase9PalindromeCheckerApp {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String input = sc.nextLine();
+        input = input.toLowerCase().replaceAll("\\s+", "");
+
+        if (isPalindrome(input, 0, input.length() - 1)) {
+            System.out.println("The given string is a Palindrome.");
+        } else {
+            System.out.println("The given string is NOT a Palindrome.");
+        }
+
+        sc.close();
+    }
+
+    public static boolean isPalindrome(String str, int left, int right) {
+        if (left >= right) {
+            return true;
+        }
+        if (str.charAt(left) != str.charAt(right)) {
+            return false;
+        }
+        return isPalindrome(str, left + 1, right - 1);
+    }
+}
