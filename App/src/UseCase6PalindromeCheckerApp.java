a299168b7250caa28c2d8049cb6f95e2513ed3cc
AppiliTeja20 <appilitejarayal81@gmail.com>
2026-02-24 14:53:37 +0530

    [AppiliTeja20] ADDED [UC6-Queue]

---
+++ App/src/UseCase6PalindromeCheckerApp.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+public class UseCase6PalindromeCheckerApp {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String input = sc.nextLine();
+        input = input.toLowerCase().replaceAll("\\s+", "");
+
+        Queue<Character> queue = new LinkedList<>();
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < input.length(); i++) {
+            char ch = input.charAt(i);
+            queue.add(ch);
+            stack.push(ch);
+        }
+
+        boolean isPalindrome = true;
+
+        while (!queue.isEmpty()) {
+            if (queue.remove() != stack.pop()) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        if (isPalindrome) {
+            System.out.println("The given string is a Palindrome.");
+        } else {
+            System.out.println("The given string is NOT a Palindrome.");
+        }
+
+        sc.close();
+    }
+}
